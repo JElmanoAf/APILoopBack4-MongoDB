50ca1becfb9f7e2c93f198f4e6371617479d8632
JElmanoAF <joao515515@hotmail.com>
2021-04-14 14:56:04 +0100

    lb4 model

---
+++ todo-list/src/models/todo.model.ts
@@ -0,0 +1,53 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+  })
+  title?: string;
+
+  @property({
+    type: 'string',
+  })
+  desc?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isComplete?: boolean;
+
+  @property({
+    type: 'string',
+  })
+  remindAtAddress?: string;
+
+  @property({
+    type: 'string',
+  })
+  remindAtGeo?: string;
+
+  @property({
+    type: 'any',
+  })
+  tag?: any;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
